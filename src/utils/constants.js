f2650ec6b3c9d5d9d6a6a956dac1d66d606194e0
Hoang Nguyen <hoangnt219@gmail.com>
2023-04-29 11:12:41 +0700

    Format price

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -40,3 +40,6 @@ export const services = [
 ];
 
 export const products_url = '/api/v1/products';
+// export const products_url = 'https://course-api.com/react-store-products'
+
+// export const single_product_url = `https://course-api.com/react-store-single-product?id=`

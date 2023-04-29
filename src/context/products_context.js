7599f3a014ed14876d90126e812ee0e3f0d57c77
Hoang Nguyen <hoangnt219@gmail.com>
2023-04-29 16:43:06 +0700

    Single product - images

--- src/context/products_context.js
+++ src/context/products_context.js
@@ -42,7 +42,6 @@ export const ProductsProvider = ({ children }) => {
     try {
       const response = await axios.get(url);
       const products = response.data.products;
-      console.log(products);
       dispatch({ type: GET_PRODUCTS_SUCCESS, payload: products });
     } catch (error) {
       dispatch({ type: GET_PRODUCTS_ERROR });
@@ -53,8 +52,7 @@ export const ProductsProvider = ({ children }) => {
     dispatch({ type: GET_SINGLE_PRODUCT_BEGIN });
     try {
       const response = await axios.get(url);
-      const product = response.data;
-      console.log(product);
+      const product = response.data.product;
       dispatch({ type: GET_SINGLE_PRODUCT_SUCCESS, payload: product });
     } catch (error) {
       dispatch({ type: GET_SINGLE_PRODUCT_ERROR });
@@ -66,7 +64,9 @@ export const ProductsProvider = ({ children }) => {
   }, []);
 
   return (
-    <ProductsContext.Provider value={{ ...state, openSidebar, closeSidebar }}>
+    <ProductsContext.Provider
+      value={{ ...state, openSidebar, closeSidebar, fetchSingleProduct }}
+    >
       {children}
     </ProductsContext.Provider>
   );
--- src/pages/SingleProductPage.js
+++ src/pages/SingleProductPage.js
@@ -1,7 +1,129 @@
-import React from 'react';
-
+import React, { useEffect } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
+import { useProductsContext } from '../context/products_context';
+import { products_url } from '../utils/constants';
+import { formatPrice } from '../utils/helpers';
+import {
+  Loading,
+  Error,
+  ProductImages,
+  AddToCart,
+  Stars,
+  PageHero,
+} from '../components';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 const SingleProductPage = () => {
-  return <div>Single</div>;
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const {
+    single_product_loading: loading,
+    single_product_error: error,
+    single_product: product,
+    fetchSingleProduct,
+  } = useProductsContext();
+
+  useEffect(() => {
+    fetchSingleProduct(`${products_url}/${id}`);
+  }, [id]);
+  useEffect(() => {
+    if (error) {
+      setTimeout(() => {
+        navigate('/');
+      }, 3000);
+    }
+  }, [error]);
+  if (loading) {
+    return <Loading />;
+  }
+  if (error) {
+    return <Error />;
+  }
+
+  const {
+    name,
+    price,
+    description,
+    stock,
+    averageRating: stars,
+    numOfReviews,
+    id: sku,
+    companyId: company,
+    primaryImage,
+    secondaryImages,
+  } = product;
+
+  console.log(product);
+  const images = secondaryImages
+    ? [primaryImage, ...secondaryImages]
+    : [primaryImage];
+  return (
+    <Wrapper>
+      <PageHero title={name} product />
+      <div className="section section-center page">
+        <Link to="/products" className="btn">
+          back to products
+        </Link>
+        <div className="product-center">
+          <ProductImages images={images} />
+          <section className="content">
+            <h2>{name}</h2>
+            <Stars stars={stars} numOfReviews={numOfReviews} />
+            <h5 className="price">{formatPrice(price)}</h5>
+            <p className="desc">{description}</p>
+            <p className="info">
+              <span>Available : </span>
+              {stock > 0 ? 'In stock' : 'out of stock'}
+            </p>
+            <p className="info">
+              <span>SKU :</span>
+              {sku}
+            </p>
+            <p className="info">
+              <span>Brand :</span>
+              {company && company.name}
+            </p>
+            <hr />
+            {/* {stock > 0 && <AddToCart product={product} />} */}
+          </section>
+        </div>
+      </div>
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.main`
+  .product-center {
+    display: grid;
+    gap: 4rem;
+    margin-top: 2rem;
+  }
+  .price {
+    color: var(--clr-primary-5);
+  }
+  .desc {
+    line-height: 2;
+    max-width: 45em;
+  }
+  .info {
+    text-transform: capitalize;
+    width: 300px;
+    display: grid;
+    grid-template-columns: 125px 1fr;
+    span {
+      font-weight: 700;
+    }
+  }
+
+  @media (min-width: 992px) {
+    .product-center {
+      grid-template-columns: 1fr 1fr;
+      align-items: center;
+    }
+    .price {
+      font-size: 1.25rem;
+    }
+  }
+`;
 
 export default SingleProductPage;

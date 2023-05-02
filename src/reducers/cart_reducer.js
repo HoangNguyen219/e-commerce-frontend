580f5251b9793515d8a3b4df6ed9a7ec41cafeea
Hoang Nguyen <hoangnt219@gmail.com>
2023-05-02 14:14:18 +0700

    AddToCart reducer - new item

--- src/reducers/cart_reducer.js
+++ src/reducers/cart_reducer.js
@@ -1,3 +1,24 @@
-const cart_reducer = () => {};
+import { ADD_TO_CART } from '../actions';
+
+const cart_reducer = (state, action) => {
+  if (action.type === ADD_TO_CART) {
+    const { id, color, amount, product } = action.payload;
+    const tempItem = state.cart.find((i) => i.id === id + color);
+    if (tempItem) {
+    } else {
+      const newItem = {
+        id: id + color,
+        name: product.name,
+        color: color,
+        amount: amount,
+        image: product.primaryImage,
+        price: product.price,
+        max: product.stock,
+      };
+      return { ...state, cart: [...state.cart, newItem] };
+    }
+  }
+  throw new Error(`No matching "${action.type}" - action type`);
+};
 
 export default cart_reducer;
